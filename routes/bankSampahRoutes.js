1858a676cfde90d2fa64b880b690297c013ed9c8
si-deltapyy <jikkajik292@gmail.com>
2025-02-20 15:26:48 +0700

    add authentication middleware to createBankSampah route

--- routes/bankSampahRoutes.js
+++ routes/bankSampahRoutes.js
@@ -5,6 +5,6 @@ const verifyToken = require('../middlewares/auth');
 
 router.get('/:id', banksampahController.getBankSampahById);
 router.get('/', verifyToken, banksampahController.getAllBankSampah);
-router.post('/', banksampahController.createBankSampah);
+router.post('/', verifyToken, banksampahController.createBankSampah);
 
 module.exports = router;
